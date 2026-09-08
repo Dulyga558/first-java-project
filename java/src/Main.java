a98c2b57d8a78aa127cf3430ec6f0d8021749961
Dulyga <dulyganurzhigituly1@gmail.com>
2026-09-08 20:35:39 +0500

    Добавлен код чекпоинта с вычислением суммы чисел

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,13 +1,9 @@
 public class Main {
-    static void process(int row, int col) {
-        System.out.println("Элемент сетки: строка " + row + ", колонка " + col);
-    }
-
     public static void main(String[] args) {
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 4; col++) {
-                process(row, col);
-            }
+        int sum = 0;
+        for (int i = 1; i <= 4; i++) {
+            sum += i;
         }
+        System.out.println("Сумма чисел: " + sum);
     }
 }

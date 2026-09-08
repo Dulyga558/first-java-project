ed18e9b92b24622f11d8941cad7c059443f75463
Dulyga <dulyganurzhigituly1@gmail.com>
2026-09-08 20:31:20 +0500

    Добавлен пример классического цикла for

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,14 +1,7 @@
 public class Main {
-
-    static int readChoice() {
-        return 3;
-    }
-
     public static void main(String[] args) {
-        int choice;
-        do {
-            choice = readChoice();
-            System.out.println("Выбран пункт меню: " + choice);
-        } while (choice < 1 || choice > 5);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
     }
 }

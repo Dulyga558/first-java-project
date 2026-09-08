6358d85da7a0fb0068f3bc694e6bcc0d64557611
Dulyga <dulyganurzhigituly1@gmail.com>
2026-09-08 20:49:39 +0500

    Добавлен пример ограничения области видимости переменных scope

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
-    static void change(int x) {
-        x = 100;
-    }
-
     public static void main(String[] args) {
-        int a = 5;
-        change(a); // әдіске тек көшірмесін береміз
-        System.out.println("Значение переменной 'a': " + a); // нәтиже бәрібір 5 болып қалады
+        int x = 10;
+        if (x > 0) {
+            int messageCount = 1;
+            System.out.println("Внутри блока: " + messageCount);
+        }
     }
 }

8b32a07d0497c757b2f9281444133ec985262e89
Dulyga <dulyganurzhigituly1@gmail.com>
2026-09-08 20:33:42 +0500

    Добавлен пример управления циклом через break и continue

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int[] a = {10, 20, 30};
-
-        // Правильный обход массива
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) continue; // 5-ті басып шығармайды, келесі қадамға өтеді
+            if (i == 8) break;    // 8-ге жеткенде цикл толығымен аяқталады
+            System.out.println(i);
         }
     }
 }

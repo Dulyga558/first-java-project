a9ed7f0b5cfb2adef3168b17b07cd483e8640cdf
Dulyga <dulyganurzhigituly1@gmail.com>
2026-09-08 20:56:35 +0500

    Добавлен пример декомпозиции на примере проверки простых чисел

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,14 +1,21 @@
 public class Main {
-    static int sum(int a, int b) {
-        return a + b;
+    static int readNumber() { return 17; }
+
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
     }
 
-    static double sum(double a, double b) {
-        return a + b;
+    static void printAnswer(int n, boolean prime) {
+        System.out.println("Число " + n + (prime ? " простое" : " составное"));
     }
 
     public static void main(String[] args) {
-        System.out.println("Сумма int: " + sum(10, 20));
-        System.out.println("Сумма double: " + sum(10.5, 20.5));
+        int n = readNumber();
+        boolean prime = isPrime(n);
+        printAnswer(n, prime);
     }
 }

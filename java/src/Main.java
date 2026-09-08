56dc60b0169fd896a24a45a7b6e44029e9c0d4c8
Dulyga <dulyganurzhigituly1@gmail.com>
2026-09-08 20:45:47 +0500

    Добавлен метод вычисления квадрата числа с параметрами

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
-    public static int max(int a, int b) {
-        if (a > b) return a;
-        return b;
+    static int square(int x) {
+        return x * x;
     }
 
     public static void main(String[] args) {
-        int result = max(15, 27);
-        System.out.println("Максимальное число: " + result);
+        int result = square(7);
+        System.out.println("Квадрат числа: " + result);
     }
 }
